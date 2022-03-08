9f6c739182a5d0427eca4e57f4d964c6435f2995
Nominprogramer <aarnav.chaplot@gmail.com>
2022-03-08 21:52:20 +0530

    added movement (pivot unstable)

--- tombstone/movement.gd
+++ tombstone/movement.gd
@@ -1,35 +1,30 @@
 extends KinematicBody
-export var max_speed = 12
-export var gravity = 70
-export var jump_impulse = 25
+
+# How fast the player moves in meters per second.
+export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+export var fall_acceleration = 75
 
 var velocity = Vector3.ZERO
-var _snap_vector := Vector3.DOWN
 
-onready var _spring_arm : SpringArm = $SpringArm
 
 func _physics_process(delta):
-	var input_vector = get_input_vector()
-	apply_movement(input_vector)
-	apply_gravity(delta)
-	velocity = move_and_slide(velocity , Vector3.UP)
+	var direction = Vector3.ZERO
 
-func get_input_vector():
-	var input_vector = Vector3.ZERO
-	input_vector.z = Input.get_action_strength("moveright") - Input.get_action_strength("moveleft")
-	input_vector.x = Input.get_action_strength("moveup") - Input.get_action_strength("movedown")
-	input_vector = input_vector.rotated(Vector3.UP, _spring_arm.rotation.y).normalized()
-	return input_vector.normalized()
-	
-func apply_movement(input_vector):
-	velocity.x = input_vector.x * max_speed
-	velocity.z = input_vector.z * max_speed
+	if Input.is_action_pressed("moveright"):
+		direction.z -= 1
+	if Input.is_action_pressed("moveleft"):
+		direction.z += 1
+	if Input.is_action_pressed("movedown"):
+		direction.x -= 1
+	if Input.is_action_pressed("moveup"):
+		direction.x += 1
 
-func apply_gravity(delta):
-	velocity.y -= gravity * delta 
-	
-var just_landed := is_on_floor() and _snap_vector == Vector3.ZERO
-var is_jumping := is_on_floor() and Input.is_action_just_pressed("jump")
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$pivot.look_at(translation + direction, Vector3.UP)
 
-func _process(_delta: float):
-	_spring_arm.translation = translation
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	velocity.y -= fall_acceleration * delta
+	velocity = move_and_slide(velocity, Vector3.DOWN)
